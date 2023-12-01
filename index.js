9ecd2529a377ff00c7b4ce429ce97c79fdbc09b3
Pablo <pablomurbaez@yahoo.com>
2023-11-30 22:31:02 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,53 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+function add() {
+    return "something";
+  }
+  function add(a, b) {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+function divideBy3() {
+  return (number /= 3);
+}
+function increment(n) {
+    return n + 1;
+  }
+  function decrement(n) {
+    return n - 1;
+  }
+  function makeInt(n) {
+    return parseInt(n);
+  }
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  function preserveDecimal(n) {
+    var parsedNumber = parseFloat(n);
+    if (isNaN(parsedNumber)) {
+    return "Invalid number";
+  } else {
+    return parsedNumber;
+  }
+}
